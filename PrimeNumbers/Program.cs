d8a7e4edc13c7d5fa22ad54e769baeb31fa40167
Sergey Zhirkov <sergzhirk@gmail.com>
2022-01-23 19:27:17 +0300

    test

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PrimeNumbers
+{
+    class PrimeNumbers
+    {
+        static void Main()
+        {
+            int num;
+            Console.Write("enter last number for finding last prime number:");
+            num = Convert.ToInt32(Console.ReadLine());
+            int divisor;
+            bool isPrime=true;
+            for (int i = 2; i <= num; i++)
+            {
+                for (divisor=2;divisor<num; divisor++)
+                {
+                    if (i % divisor == 0 && divisor!=i)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                    isPrime = true;
+                }
+                if (isPrime == true)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        }
+    }
