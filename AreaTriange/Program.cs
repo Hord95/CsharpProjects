02287ba50c964fc8e520dcd6dccdeebdb3615dc4
Sergey Zhirkov <sergzhirk@gmail.com>
2022-02-13 20:17:38 +0300

    Improving code

--- AreaTriange/Program.cs
+++ AreaTriange/Program.cs
@@ -8,7 +8,11 @@
             Console.WriteLine("Enter from 1 to 3 values of lines : ");
             var lines = Console.ReadLine();
             values = SplitAndParse(lines);
-            AreaTriange(values);
+            var area = AreaTriange(values);
+            if (area != -1)
+            {
+                Console.WriteLine(area);
+            }
         }
         public static List<double> SplitAndParse(string input)
         {
@@ -17,59 +21,41 @@
             partsInput = input.Split(' ').ToList();
             for (int i = 0; i < partsInput.Count; i++)
             {
-                if (Parse(partsInput[i]) != -1)
+                if (!string.IsNullOrWhiteSpace(partsInput[i]))
                 {
-                    values.Add(Parse(partsInput[i]));
+                    values.Add(double.Parse(partsInput[i]));
                 }
             }
             return values;
         }
-        public static double Parse(string part)
+        public static double AreaTriange(List<double> values)
         {
-            if (!String.IsNullOrEmpty(part))
+            switch (values.Count)
             {
-                var number = Convert.ToDouble(part);
-                return number;
+                case 3:
+                    {
+                        double semiperimetr = (values[0] + values[1] + values[2]) / 2;
+                        double area = Math.Sqrt(semiperimetr * (semiperimetr - values[0]) * (semiperimetr - values[1]) * (semiperimetr - values[2]));
+                        return area;
+                    }
+                case 2:
+                    {
+                        double area = values[0] * values[1] / 2;
+                        return area;
+                    }
+                case 1:
+                    {
+                        double area = ((Math.Pow(values[0], 2) * Math.Sqrt(3)) / 4);
+                        return area;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Try again!");
+                        break;
+                    }
             }
-            else
-            {
-                return -1;
-            } 
-                
-        }
-        public static void AreaTriange(List<double> values)
-        {
-            if (values.Count == 0 || values.Count > 3)
-            {
-                Console.WriteLine("Try again!");
-            }
-            else
-            {
-
-                switch (values.Count)
-                {
-                    case 3:
-                        {
-                            double semiperimetr = (values[0] + values[1] + values[2]) / 2;
-                            double area = Math.Sqrt(semiperimetr * (semiperimetr - values[0]) * (semiperimetr - values[1]) * (semiperimetr - values[2]));
-                            Console.WriteLine(area);
-                            break;
-                        }
-                    case 2:
-                        {
-                            double area = values[0] * values[1] / 2;
-                            Console.WriteLine(area);
-                            break;
-                        }
-                    case 1:
-                        {
-                            double area = ((Math.Pow(values[0], 2) * Math.Sqrt(3)) / 4);
-                            Console.WriteLine(area);
-                            break;
-                        }
-                }
-            }
+            return -1;
         }
     }
 }
-    
+
