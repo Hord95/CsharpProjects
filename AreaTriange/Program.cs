18c9e85a11d3e87a88176f88801d5bdfc8e22435
Sergey Zhirkov <sergzhirk@gmail.com>
2022-02-15 12:54:07 +0300

    update

--- AreaTriange/Program.cs
+++ AreaTriange/Program.cs
@@ -4,56 +4,46 @@
     {
         static void Main()
         {
-            Console.WriteLine("Enter from 1 to 3 values of lines : ");
+            Console.WriteLine("Enter from 1 to 3 side lenghts  : ");
             var input = Console.ReadLine();
             var values = new List<double>();
-            var partsInput = new List<string>();
-            partsInput = input.Split(' ').ToList();
-            bool isMistake = false;
-            for (int i = 0; i < partsInput.Count; i++)
-            {                
-                    double number;
-                     double.TryParse(partsInput[i],out number);
-                    if (number <= 0)
-                    {
-                        Console.WriteLine("Use numbers more than 0");
-                        isMistake = true;
-                        break;
-                    }
-                    values.Add(number);  
-            }
-            if (isMistake)
+            var partsInput = new List<string>(input.Split(' ').ToList());
+            if (partsInput.Count < 1 || partsInput.Count > 3)
             {
-                if (values.Count > 0 && values.Count <= 3)
+                Console.WriteLine("Enter from 1 to 3 side lenghts ");
+                return;
+            }
+            foreach (var part in partsInput)
+            {
+                double number;
+                if (double.TryParse(part, out number))
                 {
-                    var area = CalculateAreaTriange(values);
-                    Console.WriteLine(area);
+                    values.Add(number);
                 }
                 else
                 {
-                    Console.WriteLine("Try again");
+                    return;
                 }
             }
+            var area = CalculateAreaTriangle(values);
+            Console.WriteLine(area);
         }
-        public static double CalculateAreaTriange(List<double> values)
+        public static double CalculateAreaTriangle(List<double> values)
         {
             switch (values.Count)
             {
                 case 3:
                     {
-                        double semiperimetr = (values[0] + values[1] + values[2]) / 2;
-                        var area = Math.Sqrt(semiperimetr * (semiperimetr - values[0]) * (semiperimetr - values[1]) * (semiperimetr - values[2]));
-                        return area;
+                        double semiPerimetr = (values[0] + values[1] + values[2]) / 2;
+                        return Math.Sqrt(semiPerimetr * (semiPerimetr - values[0]) * (semiPerimetr - values[1]) * (semiPerimetr - values[2]));
                     }
                 case 2:
                     {
-                        var area = values[0] * values[1] / 2;
-                        return area;
+                        return values[0] * values[1] / 2;
                     }
                 default:
                     {
-                        var area = ((Math.Pow(values[0], 2) * Math.Sqrt(3)) / 4);
-                        return area;
+                        return ((Math.Pow(values[0], 2) * Math.Sqrt(3)) / 4);
                     }
             }
         }
