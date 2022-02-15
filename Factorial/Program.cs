ce0ea0cf1a401c8af04df9459bc5770509a1cd78
Sergey Zhirkov <sergzhirk@gmail.com>
2022-02-15 21:19:04 +0300

    Decision

---
+++ Factorial/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Factorial
+{
+    public class Factorial
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter number");
+            int number;
+            if (int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Factorial with loop = {0}", MakeFactorialWithLoop(number));
+                Console.WriteLine("Factorial without loop = {0}", MakeFactorialWithoutLoop(number));
+            }
+
+        }
+        public static int MakeFactorialWithLoop(int number)
+        {
+            var result = 1;
+            for (int i = number; i > 1; i--)
+            {
+                result *= i;
+            }
+            return result;
+        }
+        public static int MakeFactorialWithoutLoop(int number)
+        {
+            if (number > 0)
+            {
+                return number * MakeFactorialWithoutLoop(number - 1);
+            }
+            return 1;
+        }
+    }
+}
