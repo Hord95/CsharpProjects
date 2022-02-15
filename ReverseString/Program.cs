c2cbfa1d860e736cf5172cdb0c79368df4ab675e
Sergey Zhirkov <sergzhirk@gmail.com>
2022-02-15 22:29:39 +0300

    Decision

---
+++ ReverseString/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ReverseString
+{
+    class ReverseString
+    {
+        static void Main()
+        {
+            var text = Console.ReadLine();
+            var reverseText = "";
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                reverseText += text[i];
+            }
+            Console.WriteLine(reverseText);
+        }
+    }
+}
