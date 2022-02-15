8263a8820d336b13ae6281b54a9a712a8f8c9e77
Sergey Zhirkov <sergzhirk@gmail.com>
2022-02-15 22:15:03 +0300

    Decision

---
+++ Palindrome/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Palindrome
+{
+    class Palindrome
+    {
+        static void Main()
+        {
+            var word = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(word))
+            {
+                Console.WriteLine(MakePalindrome(word));
+            }
+        }
+        static string MakePalindrome(string word)
+        {
+            int i, j;
+            for (i = 0, j = word.Length - 1; i < word.Length / 2 && j > word.Length / 2 - 1; i++, j--)
+            {
+                if (word[i] != word[j])
+                {
+                    return "no";
+                }
+            }
+            return "yes";
+        }
+    }
+}
